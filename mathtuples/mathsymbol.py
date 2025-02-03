366529956ef4524aa039e364c536063733c6ca86
Frank Tompa <fwtompa@uwaterloo.ca>
2025-02-03 13:45:01 -0500

    fix element -> elem

--- mathtuples/mathsymbol.py
+++ mathtuples/mathsymbol.py
@@ -747,7 +747,7 @@ class MathSymbol:
         # for c in children:
         #     print(c.toString())
 
-        short_tag = elem.tag[element.tag.index("}")+1:]
+        short_tag = elem.tag[elem.tag.index("}")+1:]
 
         """
         ---------------------------------------------------
